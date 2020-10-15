e9d234da0c0fc0ddc3ab4a021849f94fcf403ff1
Rafyy2102 <rafpromais@yahoo.com.br>
2020-10-15 19:16:56 -0300

    Atividade de encapsulamento herança

---
+++ PacoteAtividade15_10_20/TesteAdministrador.java
@@ -0,0 +1,12 @@
+package PacoteAtividade15_10_20;
+
+public class TesteAdministrador {
+
+	public static void main(String[] args) 
+	{
+		Administrador lucas = new Administrador("Lucas", "Rua Santos", "11 41526398","45645678912", 18, 1200);
+		lucas.Informacao();
+		
+	}
+
+}
